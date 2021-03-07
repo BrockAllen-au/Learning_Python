070e80a8cd5c0f022d4ac016dd91dc43a821a2e1
b-rock-solid <brock.allen61@gmail.com>
2021-03-08 07:28:45 +1000

    Data Visualization Init

---
+++ Projects/Data Visualization/cubes.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+x_values = list(range(1, 5001))
+y_values = [x**3 for x in x_values]
+
+plt.style.use('seaborn')
+fig, ax = plt.subplots()
+ax.scatter(x_values, y_values, s=10)
+
+# Set chart title and label axes.
+ax.set_title("Cube Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Cube of Value", fontsize=14)
+
+# Set the range for each axis.
+ax.axis([0, 5100, 0, 5100**3])
+
+plt.show()
+# plt.savefig('cubes_plot.png', bbox_inches='tight')
